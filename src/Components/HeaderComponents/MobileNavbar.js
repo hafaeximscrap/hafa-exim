eb48103e7e38ba6e74340fd78068162a47833dd4
abdul <ajthameem30@gmail.com>
2024-08-11 23:30:47 +0530

    mobile wholelink click to navigate

--- src/Components/HeaderComponents/MobileNavbar.js
+++ src/Components/HeaderComponents/MobileNavbar.js
@@ -14,42 +14,52 @@ const MobileSidebar = () => {
       mobileNavbarContainer.style.height === "16rem"
     ) {
       mobileNavbarContainer.style.height = "0rem";
-    } 
+    }
   };
 
   return (
-    <div id="mobile-navbar-container" >
+    <div id="mobile-navbar-container">
       <ul id="mobile-navbar">
-        <li className="mobile-nav-list" onClick={() => HideSidebar()}>
-          <div className="mobile-nav-link-container">
-            <a href="#Home" className="mobile-nav-link">
-              Home
-            </a>
-          </div>
-        </li>
-        <li className="mobile-nav-list" onClick={() => HideSidebar()}>
-          <div className="mobile-nav-link-container">
-            <a href="#About" className="mobile-nav-link">
-              About
-            </a>
-          </div>
-        </li>
-        <li className="mobile-nav-list" onClick={() => HideSidebar()}>
-          <div className="mobile-nav-link-container">
-            <a href="#Product" className="mobile-nav-link">
-              Product
-            </a>
-          </div>
-        </li>
-        <li className="mobile-nav-list" onClick={() => HideSidebar()}>
-          <div className="mobile-nav-link-container">
-            <a href="#Contact" className="mobile-nav-link">
-              Contact
-            </a>
-          </div>
-        </li>
-      </ul>
+        <a href="#Home">
+          <li className="mobile-nav-list" onClick={() => HideSidebar()}>
+            <div className="mobile-nav-link-container">
+              <a href="#Home" className="mobile-nav-link">
+                Home
+              </a>
+            </div>
+          </li>
+        </a>
 
+        <a href="#About">
+          <li className="mobile-nav-list" onClick={() => HideSidebar()}>
+            <div className="mobile-nav-link-container">
+              <a href="#About" className="mobile-nav-link">
+                About
+              </a>
+            </div>
+          </li>
+        </a>
+
+        <a href="#Product">
+          <li className="mobile-nav-list" onClick={() => HideSidebar()}>
+            <div className="mobile-nav-link-container">
+              <a href="#Product" className="mobile-nav-link">
+                Product
+              </a>
+            </div>
+          </li>
+        </a>
+
+        <a href="#Contact">
+          <li className="mobile-nav-list" onClick={() => HideSidebar()}>
+            <div className="mobile-nav-link-container">
+              <a href="#Contact" className="mobile-nav-link">
+                Contact
+              </a>
+            </div>
+          </li>
+        </a>
+      </ul>
     </div>
   );
 };
